1cdaf8f696d8b0282e8f2c32159cd11ac5e2f3a5
Johan <jo.o.olsson@gmail.com>
2020-04-29 21:41:38 +0200

    versioning

---
+++ build/CMakeFiles/Testorosso.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Testorosso.dir/Src/Testorosso.cpp.o"
+  "lib/libTestorosso.pdb"
+  "lib/libTestorosso.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Testorosso.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/Version.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef CONFIG_H__
+#define CONFIG_H__
+
+constexpr inline int majorVersion = 0
+constexpr inline int minorVersion = 2
+
+#endif // CONFIG_H__
+
